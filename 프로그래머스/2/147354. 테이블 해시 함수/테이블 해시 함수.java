4a9c701300f15fa204eda858988252f11411cd92
hyeokeee <yuy9611@gmail.com>
2025-12-05 23:12:49 +0900

    [level 2] Title: 테이블 해시 함수, Time: 12.59 ms, Memory: 159 MB -BaekjoonHub

---
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] data, int col, int row_begin, int row_end) {
+        int answer = 0;
+        
+        Arrays.sort(data, (o1, o2) -> {
+            if(o1[col - 1] == o2[col- 1]){
+                return o2[0] - o1[0];
+            }
+            return o1[col - 1] - o2[col - 1];
+        });
+        
+        List<Integer> modSum = new ArrayList<>();
+        
+        for(int i = row_begin - 1; i < row_end; i++){
+            int sum = 0;
+            for(int j = 0; j < data[i].length; j++){
+                sum += data[i][j] % (i + 1);
+            }
+            
+            modSum.add(sum);
+        }
+        
+        answer = modSum.get(0);
+        
+        for(int i = 1; i < modSum.size(); i++){
+            answer ^= modSum.get(i);
+        }
+        
+        return answer;
+    }
+}
