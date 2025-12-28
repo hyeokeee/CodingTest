21f4b89459bc2e96f66102f7d3c498492ba73782
hyeokeee <yuy9611@gmail.com>
2025-12-28 23:42:42 +0900

    [level 2] Title: 모음 사전, Time: 0.20 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -0,0 +1,16 @@
+class Solution {
+    
+    public int solution(String word) {
+        int answer = 0;
+        int total= 3905;
+        String aeiou="AEIOU";
+        
+        for(String str: word.split("")){
+            //781, 156, 31, 6, 1
+            total/= 5;
+            answer+= total*aeiou.indexOf(str)+1;
+        }
+        
+        return answer;
+    }
+}
